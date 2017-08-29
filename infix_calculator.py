923e5c4b2f37ada3d5a9a87b0221556dc49b4d91
a-staab <astaab@gmail.com>
2017-08-28 23:08:55 -0700

    Debugged exception handling

--- infix_calculator.py
+++ infix_calculator.py
@@ -3,8 +3,9 @@ import re
 
 def calculate(input_string):
     """Takes a string of space-separated integers and operators ("*", "/", "+",
-    and "-") and computes their value following normal order-of-operations
-    associativity rules. Raises ArgumentError if invalid input is received.
+    and "-") and computes the value of expression following normal
+    order-of-operations associativity rules. Raises ArgumentError if invalid
+    input is received.
 
     >>> calculate("4 + 4")
     8
@@ -16,47 +17,66 @@ def calculate(input_string):
     10
 
     """
-    if re.match('-?\d\s[+\-*/]\s-?\d', input_string):
 
+    try:
         inputs = input_string.split(" ")
-
-        try:
-
-            while "/" in inputs:
-                operator_location = inputs.index("/")
-                subtotal = int(inputs[(operator_location - 1)]) / int(inputs[(operator_location + 1)])
-                replace_with_subtotal(inputs, operator_location, subtotal)
-
-            while "*" in inputs:
-                operator_location = inputs.index("*")
-                subtotal = int(inputs[(operator_location - 1)]) * int(inputs[(operator_location + 1)])
-                replace_with_subtotal(inputs, operator_location, subtotal)
-
-            while "-" in inputs:
-                operator_location = inputs.index("-")
-                subtotal = int(inputs[(operator_location - 1)]) - int(inputs[(operator_location + 1)])
-                replace_with_subtotal(inputs, operator_location, subtotal)
-
-            while "+" in inputs:
-                operator_location = inputs.index("+")
-                subtotal = int(inputs[(operator_location - 1)]) + int(inputs[(operator_location + 1)])
-                replace_with_subtotal(inputs, operator_location, subtotal)
-
-        except:
-            raise ArgumentError('Invalid input entered.')
-
-    else:
+    except:
         raise ArgumentError('Invalid input entered.')
 
-    result = inputs[0]
+    operators = set(["/", "*", "+", "-"])
+
+    if not operators.intersection(inputs):
+        raise ArgumentError('Invalid input entered.')
+
+    try:
+        while "/" in inputs:
+            # handle_invalid(inputs)
+            operator_location = inputs.index("/")
+            subtotal = int(inputs[(operator_location - 1)]) / int(inputs[(operator_location + 1)])
+            replace_with_subtotal(inputs, operator_location, subtotal)
+
+        while "*" in inputs:
+            # handle_invalid(inputs)
+            operator_location = inputs.index("*")
+            subtotal = int(inputs[(operator_location - 1)]) * int(inputs[(operator_location + 1)])
+            replace_with_subtotal(inputs, operator_location, subtotal)
+
+        while "-" in inputs:
+            # handle_invalid(inputs)
+            operator_location = inputs.index("-")
+            subtotal = int(inputs[(operator_location - 1)]) - int(inputs[(operator_location + 1)])
+            replace_with_subtotal(inputs, operator_location, subtotal)
+
+        while "+" in inputs:
+            # handle_invalid(inputs)
+            operator_location = inputs.index("+")
+            subtotal = int(inputs[(operator_location - 1)]) + int(inputs[(operator_location + 1)])
+            replace_with_subtotal(inputs, operator_location, subtotal)
+    except:
+        raise ArgumentError('Invalid input entered.')
+
+    if len(inputs) > 1:
+        raise ArgumentError('Invalid input entered.')
+
+    try:
+        result = int(inputs[0])
+    except:
+        raise ArgumentError('Invalid input entered.')
+
     return result
+
+
+# def handle_invalid(inputs):
+#     if not re.match('-?\d+\s[+\-*/]\s-?\d+', " ".join(inputs[:5])):
+#         raise ArgumentError('Invalid input entered.')
+#     return
 
 
 def replace_with_subtotal(inputs, operator_index, current_subtotal):
     """Reduces inputs list by removing the first operand and operator in the
     subexpression and replacing its second operand with the current subtotal.
     """
-    inputs[(operator_index + 1)] = current_subtotal
+    inputs[(operator_index + 1)] = str(current_subtotal)
     del inputs[operator_index]
     del inputs[(operator_index - 1)]
     return
@@ -67,7 +87,7 @@ class ArgumentError(Exception):
 
 # Uncomment below to run tests
 
-# if __name__ == '__main__':
-#     import doctest
-#     if doctest.testmod().failed == 0:
-#         print "\n*** All tests passed. ***\n"
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print "\n*** All tests passed. ***\n"
